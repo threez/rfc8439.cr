fd090c768067defc9b3646cb05954abe42dbb52a
Vincent Landgraf <vincent@landgrafx.de>
2024-06-29 11:25:26 +0200

    precreate encryption target

--- bench/chacha20.cr
+++ bench/chacha20.cr
@@ -4,12 +4,13 @@ require "../src/rfc8439"
 key = "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"
 nonce = "00 00 00 00 00 00 00 00 00 00 00 00"
 plaintext = Bytes.new(1024**2)
+encrypted = Bytes.new(1024**2)
 
 Benchmark.bm do |x|
   x.report("encrypt") do
     1024.times do
       chipher = Crypto::ChaCha20.new(key, nonce, 0)
-      chipher.encrypt(plaintext)
+      chipher.encrypt(plaintext, encrypted)
     end
   end
 end
